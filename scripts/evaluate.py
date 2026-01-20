Fixed stage 2 accuracy computation

scripts/evaluate.py
@@ -69,7 +69,10 @@ class evaluate:
 
     def evaluate_on(self, target="training"):
         start = time.time()
-        print("Intializing validation split...")
+        mode = target
+        if mode == "training":
+            mode = "val"
+        print(f"Intializing {mode} split...")
         dataset = GTSRBDataset(dataset_config="config/dataset.yml",
                                path_config="config/paths.yml",
                                target=target)
@@ -77,11 +80,12 @@ class evaluate:
             labels = [dataset[i][1][1] for i in range(len(dataset))]
             _, idx = train_test_split(list(range(len(dataset))), test_size=0.2, random_state=69, stratify=labels)
             dataset = Subset(dataset, idx)
-        val_loader = DataLoader(dataset, num_workers=self.max_workers, persistent_workers=True,
+        loader = DataLoader(dataset, num_workers=self.max_workers, persistent_workers=True,
                                 pin_memory=(self.device.type=="cuda"), batch_size=self.bsize)
         
-        print("Running evaluation on validation split...")
+        print(f"Running evaluation on {mode} split...")
         all_concept_preds, all_label_preds, all_concept_targets, all_label_targets = [],[],[],[]
+        wrongs = []
         with torch.no_grad():
             for i, (xb,yb) in enumerate(loader):
                 xb = xb.to(self.device, non_blocking=True)
@@ -94,12 +98,15 @@ class evaluate:
                     label_pred = logits.argmax(dim=1).detach().cpu()
                     concept_target = yb[0].detach().cpu()
                     label_target = yb[1].detach().cpu()
-                
+                for i in range(len(label_pred)):
+                    if label_pred[i] != label_target[i]:
+                        wrongs.append({"input":xb[i],"label_pred":label_pred[i],"label_target":label_target[i],
+                                       "concept_pred":concept_pred[i],"concept_target":concept_target[i]})   
                 all_concept_preds.append(concept_pred)    
                 all_label_preds.append(label_pred)
                 all_concept_targets.append(concept_target)
                 all_label_targets.append(label_target)
-                
+        print(len(wrongs))
         all_concept_preds = np.concatenate(all_concept_preds)
         all_label_preds = np.concatenate(all_label_preds)
         all_concept_targets = np.concatenate(all_concept_targets)
@@ -108,89 +115,26 @@ class evaluate:
         label_report = pd.DataFrame(metrics.classification_report(all_label_targets, all_label_preds, output_dict=True)).transpose()
         concept_accuracy = metrics.accuracy_score(all_concept_targets, all_concept_preds, normalize=True)
         label_accuracy = metrics.accuracy_score(all_label_targets, all_label_preds, normalize=True)
-        os.makedirs(f"reports/Eval-{self.ts}", exist_ok=True)
-        if target=="training":
-            concept_report.to_csv(f"reports/Eval-{self.ts}/val_concept_report.csv")
-            label_report.to_csv(f"reports/Eval-{self.ts}/val_label_report.csv")
-        else:
-            concept_report.to_csv(f"reports/Eval-{self.ts}/test_concept_report.csv")
-            label_report.to_csv(f"reports/Eval-{self.ts}/test_label_report.csv")
-        print("Concept accuracy: ",concept_accuracy)
-        print("Label accuracy: ",label_accuracy)
-        end = time.time()
-        print("Time spent: ",end-start)
-    
-    def evaluate_on_test(self):
-        start = time.time()
-        print("Initializing test dataset...")
-        test_dataset = GTSRBDataset(dataset_config="config/dataset.yml",
-                                        path_config="config/paths.yml", target="test")
-        test_loader = DataLoader(test_dataset, num_workers=self.max_workers, persistent_workers=True,
-                                 pin_memory = (self.device.type=="cuda"), batch_size=self.bsize)
-        print("Running evaluation on test dataset...")
-        all_preds = []
-        with torch.no_grad():
-            for i, (xb,yb) in enumerate(test_loader):
-                xb = xb.to(self.device, non_blocking=True)
-                with torch.autocast(device_type=self.device.type, enabled=(self.device.type=="cuda")):
-                    logits = self.instance_1(xb)
-                    concepts = torch.tensor((logits.detach().cpu() >= 0).numpy().astype(float),dtype=torch.float32)
-                    logits = self.instance_2(concepts)
-                    pred = logits.argmax(dim=1).detach().cpu()
-                all_preds.append(pred)
-        all_preds = list(np.concatenate(all_preds))
-        plots.class_distribution(all_preds,self.pth_cfg["data"]["training"],f"reports/Eval-{self.ts}")
-        end = time.time()
-        print("Time spent: ",end-start)
-
-
-
-    def evaluate_on_val(self):
-        start = time.time()
-        print("Intializing validation split...")
-        val_dataset = GTSRBDataset(dataset_config="config/dataset.yml",
-                                        path_config="config/paths.yml")
-        labels = [val_dataset[i][1][1] for i in range(len(val_dataset))]
-        _, val_idx = train_test_split(list(range(len(val_dataset))), test_size=0.2, random_state=69, stratify=labels)
-        val_dataset = Subset(val_dataset, val_idx)
-        val_loader = DataLoader(val_dataset, num_workers=self.max_workers, persistent_workers=True,
-                                pin_memory=(self.device.type=="cuda"), batch_size=self.bsize)
+        acc = pd.DataFrame({"ConceptAcc":[concept_accuracy],"LabelAcc":[label_accuracy]})
+        concept_cm = metrics.multilabel_confusion_matrix(all_label_targets,all_label_preds)
+        label_cm = metrics.confusion_matrix(all_label_targets,all_label_preds)
         
-        print("Running evaluation on validation split...")
-        all_concept_preds, all_label_preds, all_concept_targets, all_label_targets = [],[],[],[]
-        with torch.no_grad():
-            for i, (xb,yb) in enumerate(val_loader):
-                xb = xb.to(self.device, non_blocking=True)
-                yb[0] = yb[0].to(self.device, non_blocking=True)
-                yb[1] = yb[1].to(self.device, non_blocking=True)
-                with torch.autocast(device_type=self.device.type, enabled=(self.device.type=="cuda")):
-                    logits = self.instance_1(xb)
-                    concept_pred = torch.tensor((logits.detach().cpu() >= 0).numpy().astype(float), dtype=torch.float32)
-                    logits = self.instance_2(concept_pred)
-                    label_pred = logits.argmax(dim=1).detach().cpu()
-                    concept_target = yb[0].detach().cpu()
-                    label_target = yb[1].detach().cpu()
-                
-                all_concept_preds.append(concept_pred)    
-                all_label_preds.append(label_pred)
-                all_concept_targets.append(concept_target)
-                all_label_targets.append(label_target)
-                
-        all_concept_preds = np.concatenate(all_concept_preds)
-        all_label_preds = np.concatenate(all_label_preds)
-        all_concept_targets = np.concatenate(all_concept_targets)
-        all_label_targets =  np.concatenate(all_label_targets)
-        concept_report = pd.DataFrame(metrics.classification_report(all_concept_targets, all_concept_preds, output_dict=True)).transpose()
-        label_report = pd.DataFrame(metrics.classification_report(all_label_targets, all_label_preds, output_dict=True)).transpose()
-        concept_accuracy = metrics.accuracy_score(all_concept_targets, all_concept_preds, normalize=True)
-        label_accuracy = metrics.accuracy_score(all_label_targets, all_label_preds, normalize=True)
         os.makedirs(f"reports/Eval-{self.ts}", exist_ok=True)
-        concept_report.to_csv(f"reports/Eval-{self.ts}/val_concept_report.csv")
-        label_report.to_csv(f"reports/Eval-{self.ts}/val_label_report.csv")
+        
+        concept_report.to_csv(f"reports/Eval-{self.ts}/{mode}_concept_report.csv")
+        label_report.to_csv(f"reports/Eval-{self.ts}/{mode}_label_report.csv")
+        acc.to_csv(f"reports/Eval-{self.ts}/{mode}_accuracy.csv")
+        np.savetxt(f"reports/Eval-{self.ts}/{mode}_label_cm.txt", label_cm, fmt="%d")
+        f = open(f"reports/Eval-{self.ts}/{mode}_concept_cm.txt","w")
+        f.write("Per concept confusion matrix:\n\n{}\n".format(concept_cm))
+        f.close()
+        plots.class_distribution(all_label_preds,self.pth_cfg["data"]["training"],f"reports/Eval-{self.ts}")
+        
         print("Concept accuracy: ",concept_accuracy)
         print("Label accuracy: ",label_accuracy)
         end = time.time()
         print("Time spent: ",end-start)
+
 
 if __name__ == "__main__":
     p_map = {"pth_model_1":None,
@@ -205,5 +149,6 @@ if __name__ == "__main__":
                  pth_data_1=p_map["pth_data_1"], pth_data_2=p_map["pth_data_2"],
                  model_variant=p_map["model_variant"], layers=p_map["layers"])
     #e.evaluate_on_val()
-    e.evaluate_on(target="test")
+    #e.evaluate_on(target="training")
+    e.evaluate_on(target="training")
         

scripts/evaluate_old.py
@@ -15,8 +15,8 @@ if ROOT not in sys.path:
     sys.path.append(ROOT)
 
 from src.data.gtsrb_dataset import GTSRBDataset
-#from src.data.concepts_dataset import ConceptsDataset
-#from src.data.test_dataset import TestDataset
+from src.data.concepts_dataset import ConceptsDataset
+from src.data.test_dataset import TestDataset
 from src.models.ENV2 import ENV2 as Stage_1
 from src.models.LabelModel import LabelModel as Stage_2
 import src.utils.plots as plots
@@ -67,21 +67,25 @@ class evaluate:
         self.max_workers = 4+4*(self.device.type != "cuda")
         self.bsize = self.tr_cfg["stage_1"]["bsize"]
 
-    def evaluate_set(self,mode="training"):
+    def evaluate_on(self, target="training"):
         start = time.time()
-        print("Intializing validation split...")
+        mode = target
+        if mode == "training":
+            mode = "val"
+        print(f"Intializing {mode} split...")
         dataset = GTSRBDataset(dataset_config="config/dataset.yml",
-                                   path_config="config/paths.yml",
-                                   target=mode)
-        labels = [dataset[i][1][1] for i in range(len(dataset))]
-        if mode=="training":
+                               path_config="config/paths.yml",
+                               target=target)
+        if target=="training":
+            labels = [dataset[i][1][1] for i in range(len(dataset))]
             _, idx = train_test_split(list(range(len(dataset))), test_size=0.2, random_state=69, stratify=labels)
             dataset = Subset(dataset, idx)
-        loader = DataLoader(dataset, shuffle=True, num_workers=self.max_workers, persistent_workers=False,
+        loader = DataLoader(dataset, num_workers=self.max_workers, persistent_workers=True,
                                 pin_memory=(self.device.type=="cuda"), batch_size=self.bsize)
         
-        print("Running evaluation on validation split...")
+        print(f"Running evaluation on {mode} split...")
         all_concept_preds, all_label_preds, all_concept_targets, all_label_targets = [],[],[],[]
+        wrongs = []
         with torch.no_grad():
             for i, (xb,yb) in enumerate(loader):
                 xb = xb.to(self.device, non_blocking=True)
@@ -94,12 +98,15 @@ class evaluate:
                     label_pred = logits.argmax(dim=1).detach().cpu()
                     concept_target = yb[0].detach().cpu()
                     label_target = yb[1].detach().cpu()
-                
+                for i in range(len(label_pred)):
+                    if label_pred[i] != label_target[i]:
+                        wrongs.append({"input":xb[i],"label_pred":label_pred[i],"label_target":label_target[i],
+                                       "concept_pred":concept_pred[i],"concept_target":concept_target[i]})   
                 all_concept_preds.append(concept_pred)    
                 all_label_preds.append(label_pred)
                 all_concept_targets.append(concept_target)
                 all_label_targets.append(label_target)
-                
+        print(len(wrongs))
         all_concept_preds = np.concatenate(all_concept_preds)
         all_label_preds = np.concatenate(all_label_preds)
         all_concept_targets = np.concatenate(all_concept_targets)
@@ -108,25 +115,26 @@ class evaluate:
         label_report = pd.DataFrame(metrics.classification_report(all_label_targets, all_label_preds, output_dict=True)).transpose()
         concept_accuracy = metrics.accuracy_score(all_concept_targets, all_concept_preds, normalize=True)
         label_accuracy = metrics.accuracy_score(all_label_targets, all_label_preds, normalize=True)
+        acc = pd.DataFrame({"ConceptAcc":[concept_accuracy],"LabelAcc":[label_accuracy]})
+        concept_cm = metrics.multilabel_confusion_matrix(all_label_targets,all_label_preds)
+        label_cm = metrics.confusion_matrix(all_label_targets,all_label_preds)
+        
         os.makedirs(f"reports/Eval-{self.ts}", exist_ok=True)
-        if mode=="training":
-            concept_report.to_csv(f"reports/Eval-{self.ts}/val_concept_report.csv")
-            label_report.to_csv(f"reports/Eval-{self.ts}/val_label_report.csv")
-        else:
-            concept_report.to_csv(f"reports/Eval-{self.ts}/test_concept_report.csv")
-            label_report.to_csv(f"reports/Eval-{self.ts}/test_label_report.csv")
+        
+        concept_report.to_csv(f"reports/Eval-{self.ts}/{mode}_concept_report.csv")
+        label_report.to_csv(f"reports/Eval-{self.ts}/{mode}_label_report.csv")
+        acc.to_csv(f"reports/Eval-{self.ts}/{mode}_accuracy.csv")
+        np.savetxt(f"reports/Eval-{self.ts}/{mode}_label_cm.txt", label_cm, fmt="%d")
+        f = open(f"reports/Eval-{self.ts}/{mode}_concept_cm.txt","w")
+        f.write("Per concept confusion matrix:\n\n{}\n".format(concept_cm))
+        f.close()
+        plots.class_distribution(all_label_preds,self.pth_cfg["data"]["training"],f"reports/Eval-{self.ts}")
         
         print("Concept accuracy: ",concept_accuracy)
         print("Label accuracy: ",label_accuracy)
         end = time.time()
         print("Time spent: ",end-start)
 
-        
-    def evaluate_on_test(self):
-        self.evaluate_set(mode="test")
-
-    def evaluate_on_val(self):
-        self.evaluate_set(mode="training")
 
 if __name__ == "__main__":
     p_map = {"pth_model_1":None,
@@ -140,5 +148,7 @@ if __name__ == "__main__":
     e = evaluate(pth_model_1=p_map["pth_model_1"], pth_model_2=p_map["pth_model_2"], mode=p_map["mode"],
                  pth_data_1=p_map["pth_data_1"], pth_data_2=p_map["pth_data_2"],
                  model_variant=p_map["model_variant"], layers=p_map["layers"])
-    e.evaluate_on_test()
-    e.evaluate_on_val()   
+    #e.evaluate_on_val()
+    #e.evaluate_on(target="training")
+    e.evaluate_on(target="training")
+        

scripts/train.py
@@ -8,77 +8,67 @@ ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT not in sys.path:
     sys.path.append(ROOT)
 
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import TensorDataset, DataLoader, Subset, random_split
 from src.data.gtsrb_dataset import GTSRBDataset
 from src.data.concepts_dataset import ConceptsDataset
+
+from sklearn.model_selection import train_test_split
 
 from src.training.Training_Loop import Training_Loop
 import src.models.ENV2 as stage_1_models
 import src.models.LabelModel as stage_2_models
 
 class train:
-    def __init__(self, model_variant="M", early_stopping=True, configPath="config", Stage1_random_seed="", Stage2_random_seed="", Stage1_lr="", Stage2_lr="", Stage1_Epochs="", Stage2_Epochs=""):
-		self.ConfigPath = configPath
-        with open("{config}/training.yml".format(config=self.ConfigPath), "r") as f:
+    def __init__(self, model_variant="M", early_stopping=True):
+        with open("config/training.yml", "r") as f:
             self.tr_cfg = yaml.safe_load(f)
-        self.early_stopping = early_stopping
-		
-		self.Stage1_random_seed = self.tr_cfg["stage_1"]["random_seed"]
-		self.Stage2_random_seed = self.tr_cfg["stage_2"]["random_seed"]
-		
-		if Stage1_random_seed != "":
-			self.Stage1_random_seed = Stage1_random_seed
-		if Stage2_random_seed != "":
-			self.Stage2_random_seed = Stage2_random_seed
-		
-		self.Stage1_Epochs = self.tr_cfg["stage_1"]["epochs"]
-		self.Stage2_Epochs = self.tr_cfg["stage_2"]["epochs"]	
-		
-		if Stage1_Epochs != "":
-			self.Stage1_Epochs = Stage1_Epochs
-		if Stage2_Epochs != "":
-			self.Stage2_Epochs = Stage2_Epochs
-			
-		self.Stage1_lr = self.tr_cfg["stage_1"]["lr"]
-		self.Stage2_lr = self.tr_cfg["stage_2"]["lr"]	
-		
-		if Stage1_lr != "":
-			self.Stage1_lr = Stage1_lr
-		if Stage2_lr != "":
-			self.Stage2_lr = Stage2_lr
-			
-		self.model_stage_1 = stage_1_models.ENV2(model_variant=model_variant, lr=self.Stage1_lr, optimizer=self.tr_cfg["stage_1"]["optimizer"])
-        self.model_stage_2 = stage_2_models.LabelModel(lr=self.Stage2_lr, optimizer=self.tr_cfg["stage_2"]["optimizer"],
+        with open("config/dataset.yml", "r") as f:
+            self.ds_cfg = yaml.safe_load(f)
+        self.model_stage_1 = stage_1_models.ENV2(model_variant=model_variant, lr=self.tr_cfg["stage_1"]["lr"], optimizer=self.tr_cfg["stage_1"]["optimizer"])
+        self.model_stage_2 = stage_2_models.LabelModel(lr=self.tr_cfg["stage_2"]["lr"], optimizer=self.tr_cfg["stage_2"]["optimizer"],
                                                        layers=3,hidden_dim=128,hidden_dim2=64)
+        self.early_stopping = early_stopping
+
 
     def train(self):
-        self.train_1 = Training_Loop(epochs=self.Stage1_Epochs, bsize=self.tr_cfg["stage_1"]["bsize"],
+        self.train_1 = Training_Loop(epochs=self.tr_cfg["stage_1"]["epochs"], bsize=self.tr_cfg["stage_1"]["bsize"],
                                      bpdc=self.tr_cfg["stage_1"]["bpdc"], patience=self.tr_cfg["stage_1"]["patience"],
                                      min_delta=self.tr_cfg["stage_1"]["min_delta"],early_stopping=self.early_stopping,
-                                     multi_label=True, random_seed=self.Stage1_random_seed)
-        self.train_2 = Training_Loop(epochs=self.Stage2_Epochs, bsize=self.tr_cfg["stage_2"]["bsize"],
+                                     multi_label=True)
+        self.train_2 = Training_Loop(epochs=self.tr_cfg["stage_2"]["epochs"], bsize=self.tr_cfg["stage_2"]["bsize"],
                                      bpdc=self.tr_cfg["stage_2"]["bpdc"], patience=self.tr_cfg["stage_2"]["patience"],
                                      min_delta=self.tr_cfg["stage_2"]["min_delta"],early_stopping=self.early_stopping,
-                                     multi_label=False, random_seed=self.Stage2_random_seed)
+                                     multi_label=False)
         
         self.train_1.set_model(self.model_stage_1)
         self.train_2.set_model(self.model_stage_2)
         
-        dataset_1 = GTSRBDataset(dataset_config="{config}/dataset.yml".format(config=self.ConfigPath),
-                                 path_config="{config}/paths.yml"format(config=self.ConfigPath))
-        self.train_1.train(dataset_1, out_folder="stage_1")
+        dataset_1 = GTSRBDataset(dataset_config="config/dataset.yml",
+                                 path_config="config/paths.yml")
         
-        dataset_2 = self.forward_stage_1()
-        self.train_2.train(dataset_2, out_folder="stage_2")
+        total_samples = len(dataset_1)
+        labels = [dataset_1[i][1][1] for i in range(len(dataset_1))]
+        idx = list(range(len(dataset_1)))
 
-    def forward_stage_1(self):
+        train_idx, val_idx = train_test_split(list(range(len(dataset_1))), test_size=0.2, random_state=69, stratify=labels)
+        train_idx = [idx[i] for i in train_idx]
+        val_idx = [idx[i] for i in val_idx]
+
+        ds_train = Subset(dataset_1, train_idx)
+        ds_val = Subset(dataset_1, val_idx)
+        
+        self.train_1.train(ds_train, ds_val, out_folder="stage_1")
+        ds_train = self.forward_stage_1(ds_train)
+        ds_val = self.forward_stage_1(ds_val)
+        self.train_2.train(ds_train, ds_val, out_folder="stage_2")
+        
+
+    def forward_stage_1(self,dataset):
         X_in, y_in = [],[]
         instance = self.model_stage_1.get_instance()
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
-        gtsrb_ds = GTSRBDataset(dataset_config="{config}/dataset.yml".format(config=self.ConfigPath),
-                                path_config="{config}/paths.yml".format(config=self.ConfigPath))
-        loader = DataLoader(gtsrb_ds, shuffle=True, num_workers=8, persistent_workers=True,
+        loader = DataLoader(dataset, shuffle=True, num_workers=8, persistent_workers=True,
                             pin_memory = (device.type=="cuda"), batch_size=self.tr_cfg["stage_1"]["bsize"])
         
         instance.to(device, memory_format=torch.channels_last)
@@ -123,3 +113,4 @@ if __name__ == "__main__":
     x.train()
     #ds = x.forward_stage_1()
     #x.read_dataset(ds)
+    

src/training/Training_Loop.py
@@ -42,7 +42,7 @@ class Training_Loop:
         self.bsize = bsize
         self.bpdc = bpdc
         self.multi_label = multi_label
-		self.random_seed = random_seed
+        self.random_seed = random_seed
 
         self.early_stopping = early_stopping
         self.patience = patience
@@ -50,9 +50,9 @@ class Training_Loop:
 
         self.output_batches, self.output_epochs = pd.DataFrame({}),pd.DataFrame({})
         self.output_report, self.output_accuracy = pd.DataFrame({}),pd.DataFrame({})
-
+        self.output_per_label_accuracy = pd.DataFrame({})
     
-    def train(self, dataset, out_folder=""):
+    def train(self, ds_train, ds_val, out_folder=""):
         """
         Req.: A dataset is provided that the model can be trained on.
               The dataset shall provide a tuple (x,y) as sample where x is the model input and y is the target.
@@ -65,19 +65,6 @@ class Training_Loop:
         if self.model == None:
             print("Error: Model not configured")
             return
-
-        
-        
-        total_samples = len(dataset)
-        labels = [dataset[i][1][1] for i in range(len(dataset))]
-        idx = list(range(len(dataset)))
-
-        train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=0.2, self.random_seed, stratify=labels)
-        train_idx = [idx[i] for i in train_idx]
-        val_idx = [idx[i] for i in val_idx]
-
-        train_ds = Subset(dataset, train_idx)
-        val_ds = Subset(dataset, val_idx)
 
         Epoch,Batch,Train_Loss,Val_Loss = [],[],[],[]
         
@@ -93,9 +80,9 @@ class Training_Loop:
 
         instance.to(device)
 
-        train_loader = DataLoader(train_ds, shuffle=True, num_workers=max_workers, persistent_workers=True,
+        train_loader = DataLoader(ds_train, shuffle=True, num_workers=max_workers, persistent_workers=True,
                                   pin_memory = (device.type=="cuda"), batch_size=self.bsize)
-        val_loader = DataLoader(val_ds, shuffle=True, num_workers=max_workers, persistent_workers=False,
+        val_loader = DataLoader(ds_val, shuffle=True, num_workers=max_workers, persistent_workers=False,
                                 pin_memory=(device.type=="cuda"), batch_size=self.bsize)
         start = time.time()
 
@@ -173,15 +160,17 @@ class Training_Loop:
         self.output_epochs.to_csv(f"{target_folder}/Output_epochs.csv",index=False)
         self.output_report.to_csv(f"{target_folder}/Output_report.csv",index=False)
         self.output_accuracy.to_csv(f"{target_folder}/Output_accuracy.csv",index=False)
+        self.output_per_label_accuracy.to_csv(f"{target_folder}/Output_per_label_accuracy.csv", index=False)
 
         plots.epoch_loss(pd.read_csv(f"{target_folder}/Output_epochs.csv"),f"{target_folder}/Plots")
         plots.batch_loss(pd.read_csv(f"{target_folder}/Output_batches.csv"),self.bpdc, total_samples,f"{target_folder}/Plots")
         plots.report(pd.read_csv(f"{target_folder}/Output_report.csv"),f"{target_folder}/Plots")
         plots.epoch_accuracy(pd.read_csv(f"{target_folder}/Output_accuracy.csv"),f"{target_folder}/Plots")
+        plots.per_label_accuracy(pd.read_csv(f"{target_folder}/Output_per_label_accuracy.csv"), f"{target_folder}/Plots")
 
         checkpoint["last_model"] = instance.state_dict()
         checkpoint["last_optimizer"] = optimizer.state_dict()
-        torch.save(checkpoint,f"{target_folder}/{target_folder}.pth")
+        torch.save(checkpoint,f"{target_folder}/{out_folder}.pth")
 
         output_dict = {"output_batches":self.output_batches,"output_epochs":self.output_epochs,
                        "output_report":self.output_report,"output_accuracy":self.output_accuracy}
@@ -238,10 +227,11 @@ class Training_Loop:
                 all_targets.append(target)
         all_preds = np.concatenate(all_preds)
         all_targets = np.concatenate(all_targets)
-
+        
         metric_map = self.generate_metrics(all_preds, all_targets, epoch)
         self.output_report = pd.concat([self.output_report,metric_map["report"]], ignore_index=True)
         self.output_accuracy = pd.concat([self.output_accuracy,metric_map["accuracy"]], ignore_index=True)
+        self.output_per_label_accuracy = pd.concat([self.output_per_label_accuracy,metric_map["per_label_accuracy"]], ignore_index=True)
         
         return total_loss/total_samples
 
@@ -267,6 +257,11 @@ class Training_Loop:
         metric_map["report"] = report
         accuracy = metrics.accuracy_score(targets,preds,normalize=True)
         metric_map["accuracy"] = pd.DataFrame({"epoch":[epoch],"accuracy":[accuracy]})
+        label_cm = metrics.multilabel_confusion_matrix(targets, preds)
+        label_acc = {"epoch":[epoch]}
+        for i in range(len(label_cm)):
+            label_acc[str(i)] = [(label_cm[i][0][0]+label_cm[i][1][1])/(label_cm[i][0][0]+label_cm[i][0][1]+label_cm[i][1][0]+label_cm[i][1][1])]
+        metric_map["per_label_accuracy"] = pd.DataFrame(label_acc)
         return metric_map
 
     def set_model(self,model):