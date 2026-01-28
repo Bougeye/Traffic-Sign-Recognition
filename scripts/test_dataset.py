83e5f76d6f1538005eeb41b40eb3bdba920cee88
Bougeye <aaron.ziglowski@gmail.com>
2026-01-28 11:30:53 +0100

    Cleaned payloader

--- scripts/test_dataset.py
+++ scripts/test_dataset.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import yaml
 # Ensure project root is in PYTHONPATH
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(ROOT)
@@ -8,9 +8,11 @@ print(ROOT)
 from torch.utils.data import DataLoader
 from src.data.gtsrb_dataset import GTSRBDataset
 
-
-ds = GTSRBDataset(dataset_config="config/dataset.yml",
-                  path_config="config/paths.yml")
+with open("config/dataset.yml","r") as f:
+    ds_cfg = yaml.safe_load(f)
+with open("config/paths.yml", "r") as f:
+    pth_cfg = yaml.safe_load(f)
+ds = GTSRBDataset(ds_cfg,pth_cfg)
 loader = DataLoader(ds, batch_size=8, shuffle=True)
 
 for batch in loader:
